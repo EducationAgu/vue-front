f90f6cf491e57fb92d8d21a8dd7738f51b3c3d18
Буков Александр Викторович (DigitalSpirit) <abukov@digital-spirit.ru>
2021-11-12 09:51:26 +0300

    работает крипта

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,20 +1,23 @@
 import api from "./api";
 import TokenService from "./token.service";
+const bcrypt = require('bcryptjs')
 
 class AuthService {
   login({ username, password }) {
-    return api
-      .post("/auth/signin", {
-        username,
-        password
+      api.get("/auth/salt").then((response)=>{
+          const hash = bcrypt.hashSync(password, response.data)
+                  return api
+                      .post("/auth/signin", {
+                          username,
+                          "password": hash,
+                      }).then((response) => {
+                          console.log(response)
+                          if (response.data.accessToken) {
+                              TokenService.setUser(response.data);
+                          }
+                          return response.data;
+                      });
       })
-      .then((response) => {
-        if (response.data.accessToken) {
-          TokenService.setUser(response.data);
-        }
-
-        return response.data;
-      });
   }
 
   logout() {
