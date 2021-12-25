d75377982b1670a1c87895b866ac3da7733c6c00
Буков Александр Викторович (DigitalSpirit) <abukov@digital-spirit.ru>
2021-12-25 13:35:38 +0300

    https nginx

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,9 @@
 FROM nginx:1.19.0-alpine
+COPY nginx/default.conf /etc/nginx/conf.d/default.conf
+COPY nginx/nginx.conf /etc/nginx/nginx.conf
 COPY /dist /usr/share/nginx/html
-COPY ./create-cert.pem /usr/share/nginx/html
-COPY ./create-cert-key.pem /usr/share/nginx/html
-EXPOSE 80
+COPY frontend.cert /etc/nginx/frontend.cert
+COPY frontend.key /etc/nginx/frontend.key
+EXPOSE 443
 # start app
 CMD ["nginx", "-g", "daemon off;"]
--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,11 @@
-const {readFileSync} = require("fs");
+//const {readFileSync} = require("fs");
 module.exports = {
   devServer: {
     port: 8081,
-    https: {
-      key: readFileSync('./create-cert-key.pem'),
-      cert: readFileSync('./create-cert.pem')
-    },
-    hotOnly: false,
+    // https: {
+    //   key: readFileSync('./frontend.key'),
+    //   cert: readFileSync('./frontend.cert')
+    // },
+    // hotOnly: false,
   }
 }
